97cbf53e168372204e5e7b95e4a5ddb2bb59bb15
Elizabethtch <elizabethtchako@gmail.com>
2024-12-12 14:25:34 -0500

    first

--- BOOST/hello_world.py
+++ BOOST/hello_world.py
@@ -2,4 +2,5 @@
 #07/02/2024
 #Manipulate strings by stripping white space, title, captialized or lowercases string, more on formating strings 
 
-print("Hello Python world, how are you!") 
+print("Hello Python world, how are you!")
+print("hi") 
